347d7fba188930228841c596fa501b34b5ac7353
HuzefaTariq <101399969+huzefaTariq1@users.noreply.github.com>
2022-10-04 11:47:40 +0500

    Add functionality of adding course goal
    by using statemanagment

--- App.js
+++ App.js
@@ -1,15 +1,34 @@
+import { useState } from 'react';
 import { StyleSheet, Text, View, StatusBar, SafeAreaView, TextInput, Button } from 'react-native';
 
 export default function App() {
+  const [enterGoal, SetEnterGoal] = useState("")
+  const [goalList, setGoalList] = useState([])
+  const [temp, setTemp] = useState([])
+  const setInputGoal = (enterText) => {
+    SetEnterGoal(enterText)
+  }
+
+  const handleAddGoal = () => {
+    setGoalList(prevGoal => [...prevGoal, enterGoal])
+    SetEnterGoal("")
+  }
+
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.mainInputSection}>
         <TextInput style={styles.inputText}
-        placeholder='Enter Course Goal'
+          placeholder='Enter Course Goal'
+          onChangeText={setInputGoal}
+          value={enterGoal}
         />
         <Button
-        title='Add'
+          title='Add'
+          onPress={handleAddGoal}
         />
+      </View>
+      <View>
+        {goalList && goalList.map((goal, index) => <Text key={index}>{goal}</Text>)}
       </View>
     </SafeAreaView>
   );
@@ -22,16 +41,16 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0
   },
-  inputText:{
-  borderBottomColor:'black',
-  borderBottomWidth:1,
-  padding:10,
-  marginRight:3
+  inputText: {
+    borderBottomColor: 'black',
+    borderBottomWidth: 1,
+    padding: 10,
+    marginRight: 3
   },
-  mainInputSection:{
-    flexDirection:'row',
-    alignItems:"center",
-    justifyContent:"space-between"
+  mainInputSection: {
+    flexDirection: 'row',
+    alignItems: "center",
+    justifyContent: "space-between"
   }
 
 });
